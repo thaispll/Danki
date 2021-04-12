c9ef76719d84c507fbef4e13805bc032bf4c0bb6
Thais Leal <58890658+thaispll@users.noreply.github.com>
2021-04-12 12:30:10 -0300

    Add files via upload

---
+++ Form1/DIO_Javascript-master/main.js
@@ -0,0 +1,12 @@
+var nome = "Thais Leal"
+var idade = 29
+var idade2 = 10
+
+alert (nome + " tem " + idade + " anos ")
+
+//alert(idade + idade2)
+
+console.log(nome)
+console.log(n1 * n2)
+console.log(frase.toLowerCase())
+
